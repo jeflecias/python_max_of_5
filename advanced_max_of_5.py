d3ceff1773dbd47c5d5e8464094283c8d2c7366e
jeflecias <jefleciasg@gmail.com>
2024-10-14 09:57:32 +0800

    fixed more errors #2

--- advanced_max_of_5.py
+++ advanced_max_of_5.py
@@ -5,16 +5,17 @@ def find_max(array):
     #assume the first number is the highest
     highest = array[0]
 
-    for i in highest[1::]:
+    for i in array[1::]:
         if i > highest:
             highest = i
     
     return highest
 
 list_of_numbers = []
+print("Enter 5 numbers and the program will find the highest number")
 while len(list_of_numbers) != 5:
     try:
-        user_input = int(input("Enter 5 numbers and the program will find the highest number"))
+        user_input = int(input("Enter a number"))
         list_of_numbers.append(user_input)
     except ValueError:
         print("Error! Only enter numbers.")
